98968c03a37772edff11d81d4f4c6191e82ec747
AAAAAA <tdekeyser44@gmail.com>
2025-11-07 18:36:14 -0500

    Overlap Test
    Check an overlap between the player primary ability and any Actor tagged as enemy (debug Primary Ability hit box and   the actor being hit)

---
+++ Source/GASCrashCourse/Private/AbilitySystem/Abilities/Player/GCC_Primary.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright AAAAAA
+
+
+#include "AbilitySystem/Abilities/Player/GCC_Primary.h"
+
+#include "Engine/OverlapResult.h"
+
+void UGCC_Primary::HitBoxOverlapTest()
+{
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+	
+	// Ensure that the overlap test ignores the Avatar Actor
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActors(ActorsToIgnore);
+
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(HitBoxRadius);
+
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector() * HitBoxForwardOffset;
+	const FVector HitBoxLocation = GetAvatarActorFromActorInfo()->GetActorLocation() + Forward + FVector(0.f, 0.f, HitBoxElevationOffset);
+	
+	GetWorld()->OverlapMultiByChannel(OverlapResults, HitBoxLocation, FQuat::Identity, ECC_Visibility, Sphere, QueryParams, ResponseParams);
+
+	if (bDrawDebugs)
+	{
+		DrawDebugSphere(GetWorld(), HitBoxLocation, HitBoxRadius, 16, FColor::Red, false, 3.f);
+
+		for (const FOverlapResult& Result : OverlapResults)
+		{
+			if (IsValid(Result.GetActor()))
+			{
+				FVector DebugLocation = Result.GetActor()->GetActorLocation();
+				DebugLocation.Z = 200.f;
+				DrawDebugSphere(GetWorld(), DebugLocation, 30.f, 10, FColor::Green, false, 3.f);
+			}
+		}
+	}
+
+	
+}
---
+++ Source/GASCrashCourse/Private/AbilitySystem/Abilities/Player/GCC_Primary.h
@@ -0,0 +1,29 @@
+﻿// Copyright AAAAAA
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GCC_Abilities.h"
+#include "GCC_Primary.generated.h"
+
+UCLASS()
+class GASCRASHCOURSE_API UGCC_Primary : public UGCC_Abilities
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	void HitBoxOverlapTest();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxRadius = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxForwardOffset = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxElevationOffset = 20.f;
+};
